09c5f340ac7c4466726246aa100a2576da723ce6
Sean Paradiso <seanparadiso@gmail.com>
2018-10-27 18:17:23 +0000

    Add bootstrap lambda function to connect SQS metric to scaleup policy

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-zip beanstalk.zip src/beanstalk_app/beanstalk_app.py
-zip lambda.zip src/lambda/lambda.py
+zip -j beanstalk.zip src/beanstalk_app/beanstalk_app.py
+zip -j lambda.zip src/lambda/lambda.py
 
 bucket=spp-cf-artifacts
 
 aws s3 cp beanstalk.zip s3://${bucket}/
 aws s3 cp lambda.zip s3://${bucket}/
 
-aws cloudformation create-stack --stack-name lambda-bootstrap-stack --template-body file://infra/cloudformation.json
+aws cloudformation create-stack --stack-name lambda-bootstrap-stack --template-body file://infra/cloudformation.json --capabilities CAPABILITY_IAM
--- src/lambda/lambda.py
+++ src/lambda/lambda.py
@@ -1,1 +1,41 @@
-print("Hello, lambda!")
+import boto3
+import os
+
+beanstalk_client = boto3.client('elasticbeanstalk')
+autoscaling_client = boto3.client('autoscaling')
+cw_client = boto3.client("cloudwatch")
+
+beanstalk_env = os.environ["BeanstalkEnv"]
+queue_name = os.environ["QueueName"]
+
+
+def __get_asg_name():
+    resources = beanstalk_client.describe_environment_resources(EnvironmentName=beanstalk_env)
+    asg_name = resources["EnvironmentResources"]["AutoScalingGroups"][0]["Name"]
+    return asg_name
+
+
+def __get_scaling_policies(asg_name):
+    policies = autoscaling_client.describe_policies(AutoScalingGroupName=asg_name)["ScalingPolicies"]
+    scale_up_policy = [x for x in policies if x["ScalingAdjustment"] > 0][0]
+    scale_down_policy = [x for x in policies if x["ScalingAdjustment"] < 0][0]
+
+    return (scale_up_policy, scale_down_policy)
+
+
+def main(event, context):
+    asg_name = __get_asg_name()
+    scale_up_policy, scale_down_policy = __get_scaling_policies(asg_name)
+
+    cw_client.put_metric_alarm(
+        AlarmName="MyAlarm",
+        AlarmActions=[scale_up_policy["PolicyARN"]],
+        MetricName="ApproximateNumberOfMessagesVisible",
+        Namespace="AWS/SQS",
+        Dimensions=[{"Name": "QueueName", "Value": queue_name}],
+        Period=60,
+        EvaluationPeriods=1,
+        Threshold=0,
+        ComparisonOperator="GreaterThanThreshold",
+        Statistic="Maximum"
+    )
